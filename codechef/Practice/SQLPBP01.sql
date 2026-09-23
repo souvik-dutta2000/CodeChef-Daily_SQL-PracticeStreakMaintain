307430fa118ade74f777a75e7ad93f039fa908c6
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-23 12:11:22 +0530

    Solved: SQLPBP01 (codechef)

---
+++ codechef/Practice/SQLPBP01.sql
@@ -0,0 +1,9 @@
+// Problem: SQLPBP01
+// Platform: codechef
+// Language: SQL​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP01?tab=statement
+// Solved on: 2026-09-23T06:41:18.652Z
+
+-- your code goes here
+select Movie_name from cinema where rating>7 and rating<9;
