742d0b2b6a8614fa7cc66401a4320a5257095513
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-25 00:29:56 +0530

    Solved: Mark the output of following query for the below Given_table (codechef)

---
+++ codechef/Practice/SQLP49.c
@@ -0,0 +1,8 @@
+// Problem: Mark the output of following query for the below Given_table
+// Platform: codechef
+// Language: Select count(*) from Given_table;
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC05/problems/SQLP49
+// Solved on: 2026-09-24T18:59:50.790Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
