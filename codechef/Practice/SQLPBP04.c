1ba27e4f85e2e830ce5c4488f51d29e50b1cfb68
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-24 23:16:38 +0530

    Solved: SQLPBP04 (codechef)

---
+++ codechef/Practice/SQLPBP04.c
@@ -0,0 +1,18 @@
+// Problem: SQLPBP04
+// Platform: codechef
+// Language: ┌─────────────┬───────┐
+│ player_name │ score │
+├─────────────┼───────┤
+│ David       │ 1600  │
+│ Bob         │ 1500  │
+│ Charlie     │ 1300  │
+└─────────────┴───────┘
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLBP01/problems/SQLPBP04?tab=Help
+// Solved on: 2026-09-24T17:46:32.941Z
+
+SELECT DISTINCT player_name, score
+FROM Players 
+JOIN Matches ON player_name = winner
+ORDER BY score DESC
+LIMIT 3;
