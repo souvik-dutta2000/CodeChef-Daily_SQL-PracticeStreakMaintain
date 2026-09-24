fa5bb32f2a2b3e2f3730f38444e3dc855c8ba880
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-24 23:25:19 +0530

    Solved: Spot the Error in following query (codechef)

--- codechef/Practice/SQLP42.c
+++ codechef/Practice/SQLP42.c
@@ -3,6 +3,6 @@
 // Language: Select count(),user_name from Users;
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC05/problems/SQLP42
-// Solved on: 2026-09-24T17:52:41.859Z
+// Solved on: 2026-09-24T17:55:14.682Z
 
 // source not captured automatically - copy it from the editor and use Manual Push
