ed78f4e0f36efba98abbe05c27a706c6d252920c
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-24 23:22:21 +0530

    Solved: Determine the output of following Query (codechef)

---
+++ codechef/Practice/SQLP43.c
@@ -0,0 +1,8 @@
+// Problem: Determine the output of following Query
+// Platform: codechef
+// Language: select MAX(rating) from Users;
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC05/problems/SQLP43
+// Solved on: 2026-09-24T17:52:15.813Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
