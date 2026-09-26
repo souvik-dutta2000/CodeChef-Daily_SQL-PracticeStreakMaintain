dd4de2111dd5132fab79188581bfe567ade0e976
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-26 23:17:02 +0530

    Solved: SQLP07 (codechef)

---
+++ codechef/Practice/SQLP07.c
@@ -0,0 +1,13 @@
+// Problem: SQLP07
+// Platform: codechef
+// Language: SELECT
+reason
+FROM
+category_description
+WHERE
+category_id = 4;
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC01/problems/SQLP07
+// Solved on: 2026-09-26T17:47:00.870Z
+
+// source not captured automatically - copy it from the editor and use Manual Push
