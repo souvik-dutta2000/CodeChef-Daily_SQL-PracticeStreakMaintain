43b6ce3d7b96c803be85d900ef8e3438c7b6ab0d
SOUVIK DUTTA <71960578+souvik-dutta2000@users.noreply.github.com>
2026-09-26 23:17:24 +0530

    Solved: SQLP07 (codechef)

--- codechef/Practice/SQLP07.c
+++ codechef/Practice/SQLP07.c
@@ -8,6 +8,6 @@ WHERE
 category_id = 4;
 // Verdict: Accepted
 // URL: https://www.codechef.com/practice/course/sql-case-studies-topic-wise/SQLPRAC01/problems/SQLP07
-// Solved on: 2026-09-26T17:47:00.870Z
+// Solved on: 2026-09-26T17:47:23.509Z
 
 // source not captured automatically - copy it from the editor and use Manual Push
